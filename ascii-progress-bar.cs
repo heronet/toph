0b4dced9df5c83e6d2dd5eca2fd85c1bd3372ede
heronet <sirat4757@gmail.com>
2021-03-22 21:17:06 +0600

    ascii-progress-bar.cs

---
+++ ascii-progress-bar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Toph
+{
+    class AsciiProgressBar
+    {
+        static void Main(string[] args)
+        {
+            double input = Double.Parse(Console.ReadLine());
+            int pluses = (int)(input / 10);
+
+            string bar = "[";
+            for (int i = 0; i != pluses; ++i)
+                bar += '+';
+            for (int t = 0; t != 10 - pluses; ++t)
+                bar += '.';
+            bar += ']';
+            Console.WriteLine($"{bar} {(int)input}%");
+        }
+    }
+}
